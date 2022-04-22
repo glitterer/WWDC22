ac04eb7a64f743160eacb42b4530c2c00078aceb
Seik Oh <seikoh@naver.com>
2022-04-22 23:23:39 +0900

    add animation(?) sentence

--- EssayWriting.swiftpm/App/EssayWritingtView.swift
+++ EssayWriting.swiftpm/App/EssayWritingtView.swift
@@ -2,17 +2,56 @@ import SwiftUI
 
 struct EssayWritingtView: View {
     
+    let timer = Timer.publish(every: 1.5, on: .main, in: .common).autoconnect()
+    @State var count: Float = 4.5
+    @State var finishedText: String? = nil //optional string
+    
+    //Animation Properties
+    @State var isAnimating = false
+    @State var startAnimation = false
+    @State var isFinished = false
+    
     var body: some View {
-        VStack{
-            Text("[ TODO: Let's put a pencil character image here ]")
-                .font(.title)
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
-            
-            Text("✏️This is an app to study how to write a 5 paragraph essay!✏️")
-                .font(.title)
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
+        ZStack{
+            VStack{
+                Text("[ TODO: Let's put a pencil character image here ]")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                
+                Text(finishedText ?? "") //if no finished string, insert count
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .opacity(startAnimation ? 1: 0)
+            }
+            .onReceive(timer, perform: {_ in
+                if count == 4.5{
+                    finishedText = "✏️This is an app✏️"
+                    count -= 1.5
+                }
+                else if count == 3.0 {
+                    finishedText = "✏️This is an app to study how to write✏️"
+                    count -= 1.5
+                }
+                else {
+                    finishedText = "✏️This is an app to study how to write a 5 paragraph essay!✏️"
+                    count -= 1.5
+                }
+            })
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+                withAnimation(.spring()){  //.linear(duration: 2)
+                    startAnimation.toggle()
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4.5){
+                withAnimation{
+                    isFinished.toggle()
+                }
+            }
         }
     }
 }
+
