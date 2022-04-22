f55fd4725fef2a500cf4a14ac300f044054b1afa
Seik Oh <seikoh@naver.com>
2022-04-22 21:11:55 +0900

    [ADD] EssayWriting playgroun project

---
+++ EssayWriting.swiftpm/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Creating a new app to teach how to write a 5 paragraph essay!")
+        }
+    }
+}
