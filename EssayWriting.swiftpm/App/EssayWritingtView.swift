a137e46e8c29005074a71e40d712d0c6b09be003
Seik Oh <seikoh@naver.com>
2022-04-22 23:07:57 +0900

    [ADD] SplashScreen

---
+++ EssayWriting.swiftpm/App/EssayWritingtView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct EssayWritingtView: View {
+    
+    var body: some View {
+        VStack{
+            Text("[ TODO: Let's put a pencil character image here ]")
+                .font(.title)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+            
+            Text("✏️This is an app to study how to write a 5 paragraph essay!✏️")
+                .font(.title)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
---
+++ EssayWriting.swiftpm/App/SplashView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActive:Bool = false
+    
+    var body: some View {
+        VStack {
+            if self.isActive {
+                EssayWritingtView()
+            } else {
+                Image("wwdc22logo")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+                    
+                Text("Essay Writing!")
+                    .font(Font.largeTitle)
+                
+                //TODO: Add scripling or typing sound effect
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
